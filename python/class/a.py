7bcbc13895e8e03468ae4cb6b01c13873f0780cb
shashank <iamshashankpeddi@gmail.com>
2020-12-28 19:39:14 +0530

    cleared some files

--- python/class/a.py
+++ python/class/a.py
@@ -1,5 +1,1 @@
-from openpyxl import *
-wb = load_workbook(r"C:\vscode\Book2.xlsx")
-sheet = wb.active
-sheet['A1'] = 'shashank'
-wb.save(r"C:\vscode\Book1.xlsx")
+
--- python/class/game.py
+++
@@ -1,9 +0,0 @@
-x = int(input('min :'))
-y = int(input('max :'))
-for i in range(x ,y ):
-    if i%2 != 0:
-        print('odd numbers')
-        print(i)
-    if i%2 == 0:
-        print('even numbers')
-        print(i)
