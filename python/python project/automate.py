85feb595bd013d32135b379c4f82676c85874785
shashank <iamshashankpeddi@gmail.com>
2021-01-06 11:50:26 +0530

    deleted some files

---
+++ python/python project/automate.py
@@ -0,0 +1,11 @@
+import pyautogui 
+import time
+time.sleep(2)
+pyautogui.keyDown("s")
+pyautogui.keyDown("h")
+pyautogui.keyDown("a")
+pyautogui.keyDown("s")
+pyautogui.keyDown("h")
+pyautogui.keyDown("a")
+pyautogui.keyDown("n")
+pyautogui.keyDown("k")
---
+++ python/python project/flask/web with fask.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/shashank')
+def shashank():
+    return 'HelloWorld!'
+
+app.run(debug=True)
